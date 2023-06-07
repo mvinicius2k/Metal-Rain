1bbefb6398c599855acc711195d0b7ccf1e0e94b
Marcos Vinícius <marcos.vinicius124816@gmail.com>
2023-06-07 14:57:21 -0300

    refatoração de radar

--- Assets/DOTS Scripts/Systems/RadarSystem.cs
+++ Assets/DOTS Scripts/Systems/RadarSystem.cs
@@ -8,6 +8,7 @@ using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Physics.Systems;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 [
     BurstCompile,
@@ -59,13 +60,18 @@ public partial struct RadarSystem : ISystem
         var redSingleton = SystemAPI.GetSingleton<BeginFixedStepSimulationEntityCommandBufferSystem.Singleton>();
         var redEcb = redSingleton.CreateCommandBuffer(state.WorldUnmanaged);
 
+        var greenTanksCount = greenTanks.CalculateEntityCount();
+        var redTanksCount = redTanks.CalculateEntityCount();
+
         var redJob = new TankRadarJob
         {
-            Enemies = greenTanks.ToArchetypeChunkArray(Allocator.TempJob),
+            EnemiesChunks = greenTanks.ToArchetypeChunkArray(Allocator.TempJob),
             Ecb = redEcb.AsParallelWriter(),
             Random = new Unity.Mathematics.Random(50),
             TankAspectTypeHandle = new TankAspect.TypeHandle(ref state),
-            Accuracy = math.min(TankAccuracy, greenTanks.CalculateEntityCount())
+            Accuracy = math.min(TankAccuracy, greenTanksCount),
+            EnemiesCount = greenTanksCount,
+            
 
         }.ScheduleParallel(freeRedTanks, state.Dependency);
 
@@ -76,11 +82,12 @@ public partial struct RadarSystem : ISystem
 
         var greenJob = new TankRadarJob
         {
-            Enemies = redTanks.ToArchetypeChunkArray(Allocator.TempJob),
+            EnemiesChunks = redTanks.ToArchetypeChunkArray(Allocator.TempJob),
             Ecb = greenEcb.AsParallelWriter(),
             Random = new Unity.Mathematics.Random(50),
             TankAspectTypeHandle = new TankAspect.TypeHandle(ref state),
-            Accuracy = math.min(TankAccuracy, redTanks.CalculateEntityCount())
+            Accuracy = math.min(TankAccuracy, redTanksCount),
+            EnemiesCount = redTanksCount
 
         }.ScheduleParallel(freeGreenTanks, redJob);
 
@@ -101,16 +108,74 @@ public struct AimTarget
     
 }
 
-[BurstCompile]
+
+
+//    [BurstCompile]
 public partial struct TankRadarJob : IJobChunk
 {
     [ReadOnly]
-    public NativeArray<ArchetypeChunk> Enemies;
+    public NativeArray<ArchetypeChunk> EnemiesChunks;
     public TankAspect.TypeHandle TankAspectTypeHandle;
     public Unity.Mathematics.Random Random;
     public EntityCommandBuffer.ParallelWriter Ecb;
     public int Accuracy;
+    public int EnemiesCount;
 
+    //private NativeArray<AimTarget> EnemyDistances;
+    public NativeArray<AimTarget> RadarTank(TankAspect tank, int sortkey = 0)
+    {
+        //var targets = new NativeArray<AimTarget>(Accuracy, Allocator.Temp);
+        var startPosition = tank.LocalTransform.ValueRO.Position;
+        var enemyDistances = new NativeArray<AimTarget>(EnemiesCount, Allocator.Temp);
+        var flatIndex = 0;
+        for (int i = 0; i < EnemiesChunks.Length; i++)
+        {
+            var enemies = TankAspectTypeHandle.Resolve(EnemiesChunks[i]);
+            for (int j = 0; j < enemies.Length; j++)
+            {
+                var distance = math.distance(startPosition, enemies[j].Position);
+                enemyDistances[flatIndex++] = new AimTarget
+                {
+                    Distance = distance,
+                    Entity = enemies[j].Entity,
+                    Position = enemies[j].Position
+                };
+
+                //for (int k = 0; k < targets.Length; k++)
+                //{
+                //    if (distance < targets[k].Distance || targets[k].Entity == Entity.Null)
+                //    {
+                //        targets[k] = new AimTarget { 
+                //            Distance = distance,
+                //            Position = enemiesChunk[j].Position,
+                //            Entity = enemiesChunk[j].Entity
+                //        };
+
+                //        targets.Sort(new DistanceComparer());
+                //        break;
+                //    }
+                //}
+            }
+
+        }
+
+        enemyDistances.Sort(new DistanceComparer());
+        return enemyDistances;
+        //var index = Random.NextInt(0, targets.Length);
+        //var target = targets[index];
+
+
+        //tank.SetAimTo(target.Position); //Move a malha
+        //                                //Ecb.SetComponent(unfilteredChunkIndex, tank.ModelEntity, new ModelLookAt { Target = target.Position });
+        //                                //Ecb.SetComponentEnabled<ModelLookAt>(unfilteredChunkIndex, tank.ModelEntity, true);
+
+        //Ecb.SetComponentEnabled<TankAttack>(unfilteredChunkIndex, tank.Entity, true); //Ativa o componenete de ataque, que vai ser processado em AttackSystem
+        //                                                                              //Registra o tanque inimigo escolhido para mirar
+        //tank.Attack.ValueRW.Target = target.Entity;
+
+        ////Não é mais um tanque livre
+        //Ecb.SetComponentEnabled<StandbyTankTag>(unfilteredChunkIndex, tank.Entity, false);
+    }
     [BurstCompile]
     public void Execute(in ArchetypeChunk chunk, int unfilteredChunkIndex, bool useEnabledMask, in v128 chunkEnabledMask)
     {
@@ -119,42 +184,50 @@ public partial struct TankRadarJob : IJobChunk
         for (int tankIndex = 0; tankIndex < tanks.Length; tankIndex++)
         {
             var tank = tanks[tankIndex];
-            var targets = new NativeArray<AimTarget>(Accuracy, Allocator.Temp);
-            var startPosition = tank.LocalTransform.ValueRO.Position;
+            var enemies = RadarTank(tank);
 
-            for (int i = 0; i < Enemies.Length; i++)
-            {
-                var enemyChunk = Enemies[i];
 
-                var enemiesChunk = TankAspectTypeHandle.Resolve(enemyChunk);
+            ////EnemyDistances = new NativeArray<AimTarget>(EnemiesCount, Allocator.Temp);
 
-                for (int j = 0; j < enemiesChunk.Length; j++)
-                {
-                    var distance = math.distance(startPosition, enemiesChunk[j].Position);
-                    for (int k = 0; k < targets.Length; k++)
-                    {
-                        if (distance < targets[k].Distance || targets[k].Entity == Entity.Null)
-                        {
-                            targets[k] = new AimTarget { 
-                                Distance = distance,
-                                Position = enemiesChunk[j].Position,
-                                Entity = enemiesChunk[j].Entity
-                            };
-                            
-                            targets.Sort(new DistanceComparer());
-                            break;
-                        }
-                    }
-                }
+            ////var targets = new NativeArray<AimTarget>(Accuracy, Allocator.Temp);
+            //var startPosition = tank.LocalTransform.ValueRO.Position;
 
-            }
-            var index = Random.NextInt(0, targets.Length);
-            var target = targets[index];
+            //var enemyDistances = new NativeArray<AimTarget>(EnemiesChunks.Length, Allocator.Temp);
+            //for (int i = 0; i < EnemiesChunks.Length; i++)
+            //{
+            //    var enemyChunk = EnemiesChunks[i];
+            //    var enemiesChunk = TankAspectTypeHandle.Resolve(enemyChunk);
+
+            //    for (int j = 0; j < enemiesChunk.Length; j++)
+            //    {
+            //        var distance = math.distance(startPosition, enemiesChunk[j].Position);
+
+            //        //for (int k = 0; k < targets.Length; k++)
+            //        //{
+            //        //    if (distance < targets[k].Distance || targets[k].Entity == Entity.Null)
+            //        //    {
+            //        //        targets[k] = new AimTarget { 
+            //        //            Distance = distance,
+            //        //            Position = enemiesChunk[j].Position,
+            //        //            Entity = enemiesChunk[j].Entity
+            //        //        };
+
+            //        //        targets.Sort(new DistanceComparer());
+            //        //        break;
+            //        //    }
+            //        //}
+            //    }
+
+            //}
+
+
+
+           // var ceil = math.min(enemies.Length, Accuracy);
+            var index = Random.NextInt(0, Accuracy);
+            var target = enemies[index];
 
 
             tank.SetAimTo(target.Position); //Move a malha
-            //Ecb.SetComponent(unfilteredChunkIndex, tank.ModelEntity, new ModelLookAt { Target = target.Position });
-            //Ecb.SetComponentEnabled<ModelLookAt>(unfilteredChunkIndex, tank.ModelEntity, true);
 
             Ecb.SetComponentEnabled<TankAttack>(unfilteredChunkIndex, tank.Entity, true); //Ativa o componenete de ataque, que vai ser processado em AttackSystem
             //Registra o tanque inimigo escolhido para mirar
